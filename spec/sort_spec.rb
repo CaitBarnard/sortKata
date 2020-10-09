4c858927d027ccea99ae57269158fcfd7ad6a946
cait <caitlin@madetech.com>
2020-10-09 14:49:44 +0100

    Sorts an array of 2 numbers

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -1,4 +1,11 @@
 def sort(array)
+  length = array.size
+  return array if length <= 1
+
+  if array[0] > array[1]
+    array[0], array[1] = array[1], array[0]
+  end
+
   array
 end
 
@@ -8,5 +15,12 @@ describe "sorting" do
   it "returns an array" do
     array = []
     expect(sort(array)).to be_a(Array)
+    expect(sort(array)).to eq([])
   end
+
+  it "sorts an array of 2 numbers" do
+    array = [2,1]
+    expect(sort(array)).to eq([1,2])
+  end
+  
 end
