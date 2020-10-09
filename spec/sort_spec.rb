a455ac55f80fef596038c4e539a446f0db65f9a9
cait <caitlin@madetech.com>
2020-10-09 15:17:09 +0100

    Implement bubblesort

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -2,10 +2,17 @@ def sort(array)
   length = array.size
   return array if length <= 1
 
-  if array[0] > array[1]
-    array[0], array[1] = array[1], array[0]
+  loop do
+    swapped = false
+    (length-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+    break if not swapped
   end
-
+  
   array
 end
 
@@ -22,5 +29,14 @@ describe "sorting" do
     array = [2,1]
     expect(sort(array)).to eq([1,2])
   end
+
+  it "sorts an array of 3 numbers" do
+    array = [3,2,1]
+    expect(sort(array)).to eq([1,2,3])
+  end
   
+  it "sorts an array of duplicate numbers" do
+    array = [2,1,1,2]
+    expect(sort(array)).to eq([1,1,2,2])
+  end
 end
